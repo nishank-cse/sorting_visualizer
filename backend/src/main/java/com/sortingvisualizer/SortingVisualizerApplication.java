263bcbb262c01a1f82d38eb3cd8baefecf46a439
Nishank Mukhija <nishankmukhija.cse@gmail.com>
2026-04-25 10:38:57 +0530

    docker setup

--- backend/src/main/java/com/sortingvisualizer/SortingVisualizerApplication.java
+++ backend/src/main/java/com/sortingvisualizer/SortingVisualizerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // 👇 add this import
-import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class SortingVisualizerApplication {
@@ -12,8 +12,8 @@ public class SortingVisualizerApplication {
     public static void main(String[] args) {
 
         // 👇 load .env file
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+        
+        
 
         SpringApplication.run(SortingVisualizerApplication.class, args);
     }
